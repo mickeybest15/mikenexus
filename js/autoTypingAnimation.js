458f60efc9c4d74aecc11f8f872fc825cc02699e
mickeybest15 <107423281+mickeybest15@users.noreply.github.com>
2023-06-15 22:29:53 +0100

    Update autoTypingAnimation.js

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,7 +1,9 @@
 setTimeout(function() {
     $('.about-info-inner').typed({
       strings: [
-        "<span>Hi, I am Golam Morsed</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit eius officia ea ut vonsectetur adipisicing elit. Illo voluptatum iste, tempora rem obcaecati omnis delectus sunt, consequuntur, et ut quasi aperiam assumenda! <br><br> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex beatae reprehenderit vero maiores dignissimos error vitae, optio inventore explicabo quia officiis magni delectus? Lorem ipsum dolor, sit amet consectetur"
+        "<span>Hi, I am Mike Agunloye </span> Hello, I am MikeNexus, and I make impactful, effective, and high-quality explainer videos for businesses and saas products I also make logo animations that can be used on websites, youtube videos, and any other relevant platforms. I have 6 years professional experience working outside fiverr for various creative agencies and each time exceeding the clients expectations. I'm highly skilled in After effects, adobe illustrator and adobe premier pro. I am keen on listening and understanding the client needs so as to deliver the best.
+
+"
       ],
       typeSpeed: 7,
       contentType: 'html'
