9fed2b61e387176817956fcd7749c99e8bc27b8e
mickeybest15 <107423281+mickeybest15@users.noreply.github.com>
2023-06-16 09:23:20 +0100

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Mike Agunloye", "Freelancer", "Web developer", "Problem solver", "Saas Animation", "Video Editor", "Mobile App Developer"];
+const textArray = ["MIKE AGUNLOYE", "FREELANCER", "WEB DEVELOPER", "FIGMA UI\UX DESIGNER", "SAAS ANIMATOR", "VIDEO EDITOR", "MOBILE APP DEVELOPER"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
