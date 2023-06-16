a1f0a80047bc3f92f4fae1bc46bf3c138b9b8440
mickeybest15 <107423281+mickeybest15@users.noreply.github.com>
2023-06-16 10:13:11 +0100

    Update autoTypingAnimation.js

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
     $('.about-info-inner').typed({
       strings: [
-        "<span>Hi, I am Mike Agunloye </span> Hello, I am MikeNexus, and I make impactful, effective, and high-quality explainer videos for businesses and saas products I also make logo animations that can be used on websites, youtube videos, and any other relevant platforms. I have 6 years professional experience working outside fiverr for various creative agencies and each time exceeding the clients expectations. I'm highly skilled in After effects, adobe illustrator and adobe premier pro. I am keen on listening and understanding the client needs so as to deliver the best."],
+        "<span>Hi, I am Mike Agunloye </span> I am a full-stack Web developer, UI\UX Designer and Video Editor for businesses and saas products. I have 4 years of experience in this field. My skill includes different CMS's(Content management systems). These CMS include Bubble io, WordPress, Shopify, Wix, Go Daddy, and Squarespace. The languages I have learned are HTML5, CSS3, JavaScript, and many more. I can develop or design any type of Website like Business, Ecommerce, Portfolio, Affiliated, and multivendor. I am keen on listening and understanding the client needs so as to deliver the best."],
       typeSpeed: 7,
       contentType: 'html'
     });
